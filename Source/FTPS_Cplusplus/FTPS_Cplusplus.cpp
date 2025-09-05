f65fde9305eecae2638a99e901edfc09d91287de
Kevin Armando Kurumilla Moreno <kevin.kurumilla@hotmail.com>
2025-09-04 17:55:02 -0700

    Initial Project

---
+++ Source/FTPS_Cplusplus/FTPS_Cplusplus.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FTPS_Cplusplus.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FTPS_Cplusplus, "FTPS_Cplusplus" );
